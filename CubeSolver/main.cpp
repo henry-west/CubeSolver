d4b4ad33d421d42817b8d01d3bc1934c2f8d9945
Henry West <henrywest93@outlook.com>
2014-09-16 11:00:21 -0500

    Initial Commit

---
+++ CubeSolver/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CubeSolver
+//
+//  Created by Henry West on 9/16/14.
+//  Copyright (c) 2014 Henry West. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
